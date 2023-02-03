d9460596a51d143086cdbc9c076afdab335f827b
OverEuro <yangyijun94@163.com>
2023-02-03 20:19:41 +1100

    stable dictionary learning

--- jaxrl/dict_learning/task_dict.py
+++ jaxrl/dict_learning/task_dict.py
@@ -25,95 +25,6 @@ def overlap_3(arr1, arr2):
     return n_intersected / n_union
 
 
-def _update_dict(
-    dictionary,
-    Y,
-    code,
-    A=None,
-    B=None,
-    c=1e-1,
-    verbose=False,
-    random_state=None,
-    positive=False,
-):
-    """Update the dense dictionary factor in place.
-
-    Parameters
-    ----------
-    dictionary : ndarray of shape (n_components, n_features)
-        Value of the dictionary at the previous iteration.
-
-    Y : ndarray of shape (n_samples, n_features)
-        Data matrix.
-
-    code : ndarray of shape (n_samples, n_components)
-        Sparse coding of the data against which to optimize the dictionary.
-
-    A : ndarray of shape (n_components, n_components), default=None
-        Together with `B`, sufficient stats of the online model to update the
-        dictionary.
-
-    B : ndarray of shape (n_features, n_components), default=None
-        Together with `A`, sufficient stats of the online model to update the
-        dictionary.
-
-    verbose: bool, default=False
-        Degree of output the procedure will print.
-
-    random_state : int, RandomState instance or None, default=None
-        Used for randomly initializing the dictionary. Pass an int for
-        reproducible results across multiple function calls.
-        See :term:`Glossary <random_state>`.
-
-    positive : bool, default=False
-        Whether to enforce positivity when finding the dictionary.
-
-        .. versionadded:: 0.20
-    """
-    n_samples, n_components = code.shape
-    random_state = check_random_state(random_state)
-
-    if A is None:
-        A = code.T @ code
-    if B is None:
-        B = Y.T @ code
-
-    n_unused = 0
-    for k in range(n_components):
-        if A[k, k] > 1e-6:
-            # 1e-6 is arbitrary but consistent with the spams implementation
-            # -np.inf means that never resample atoms.
-            dictionary[k] += (B[:, k] - A[k] @ dictionary) / A[k, k]
-        else:
-            # kth atom is almost never used -> sample a new one from the data
-            # newd = Y[random_state.choice(n_samples)]
-
-            # add small noise to avoid making the sparse coding ill conditioned
-            # noise_level = 0.01 * (newd.std() or 1)  # avoid 0 std
-            # noise = random_state.normal(0, noise_level, size=len(newd))
-            # dictionary[k] = newd + noise
-
-            # randomly initialize kth atom
-            # scale = 1.0 * (newd.std() or 1)  # avoid 0 std
-            # dictionary[k] = random_state.normal(loc=0.0, scale=scale, size=len(newd))
-            # code[:, k] = 0
-            # n_unused += 1
-
-            pass
-
-        if positive:
-            np.clip(dictionary[k], 0, None, out=dictionary[k])
-
-        # Projection on the constraint set ||V_k|| <= c
-        dictionary[k] /= max(np.linalg.norm(dictionary[k]), 1)
-        dictionary[k] *= c
-
-    if verbose and n_unused > 0:
-        print(f"{n_unused} unused atoms resampled.")
-
-    return dictionary
-
-
 class BasisVectorLearner(object):
 
     def __init__(self, 
@@ -238,6 +149,97 @@ class OnlineDictLearner(object):
     def get_next_codes(self):
         return np.zeros((1, self._n_components))
 
+
+def _update_dict(
+    dictionary,
+    Y,
+    code,
+    A=None,
+    B=None,
+    c=1e-1,
+    verbose=False,
+    random_state=None,
+    positive=False,
+):
+    """Update the dense dictionary factor in place.
+
+    Parameters
+    ----------
+    dictionary : ndarray of shape (n_components, n_features)
+        Value of the dictionary at the previous iteration.
+
+    Y : ndarray of shape (n_samples, n_features)
+        Data matrix.
+
+    code : ndarray of shape (n_samples, n_components)
+        Sparse coding of the data against which to optimize the dictionary.
+
+    A : ndarray of shape (n_components, n_components), default=None
+        Together with `B`, sufficient stats of the online model to update the
+        dictionary.
+
+    B : ndarray of shape (n_features, n_components), default=None
+        Together with `A`, sufficient stats of the online model to update the
+        dictionary.
+
+    verbose: bool, default=False
+        Degree of output the procedure will print.
+
+    random_state : int, RandomState instance or None, default=None
+        Used for randomly initializing the dictionary. Pass an int for
+        reproducible results across multiple function calls.
+        See :term:`Glossary <random_state>`.
+
+    positive : bool, default=False
+        Whether to enforce positivity when finding the dictionary.
+
+        .. versionadded:: 0.20
+    """
+    n_samples, n_components = code.shape
+    random_state = check_random_state(random_state)
+
+    if A is None:
+        A = code.T @ code
+    if B is None:
+        B = Y.T @ code
+
+    n_unused = 0
+    for k in range(n_components):
+        if A[k, k] > 1e-6:
+            # 1e-6 is arbitrary but consistent with the spams implementation
+            # -np.inf means that never resample atoms.
+            dictionary[k] += (B[:, k] - A[k] @ dictionary) / A[k, k]
+        else:
+            # kth atom is almost never used -> sample a new one from the data
+            newd = Y[random_state.choice(n_samples)]
+
+            # add small noise to avoid making the sparse coding ill conditioned
+            noise_level = 1.0 * (newd.std() or 1)  # avoid 0 std
+            noise = random_state.normal(0, noise_level, size=len(newd))
+            dictionary[k] = newd + noise
+            code[:, k] = 0
+            n_unused += 1
+
+            # randomly initialize kth atom
+            # scale = 1.0 * (newd.std() or 1)  # avoid 0 std
+            # dictionary[k] = random_state.normal(loc=0.0, scale=scale, size=len(newd))
+            # code[:, k] = 0
+            # n_unused += 1
+
+            # pass
+
+        if positive:
+            np.clip(dictionary[k], 0, None, out=dictionary[k])
+
+        # Projection on the constraint set ||V_k|| <= c
+        dictionary[k] /= max(np.linalg.norm(dictionary[k]), 1)
+        dictionary[k] *= c
+
+    if verbose and n_unused > 0:
+        print(f"{n_unused} unused atoms resampled.")
+
+    return dictionary
+
     
 class OnlineDictLearnerV2(object):
     def __init__(self, 
@@ -250,7 +252,7 @@ class OnlineDictLearnerV2(object):
                  method: str='lasso_lars', # ['lasso_cd', 'lasso_lars', 'threshold']
                  positive_code: bool=False,
                  scale_code: bool=False,
-                 verbose=False):
+                 verbose=True):
 
         self.N = 0
         self.rng = np.random.RandomState(seed=seed)
@@ -270,13 +272,13 @@ class OnlineDictLearnerV2(object):
         self.method = method
         self.archives = None
         self._verbose = verbose
-        self.arch_gates = None
+        self.arch_code = None
         self._positive_code = positive_code
-        self.scale_code = scale_code
+        self._scale_code = scale_code
         self.change_of_dict = []
         
-    def get_gates(self, sample: np.ndarray):
-        gates = sparse_encode(
+    def get_alpha(self, sample: np.ndarray):
+        code = sparse_encode(
             sample,
             self.D,
             algorithm=self.method, 
@@ -286,30 +288,30 @@ class OnlineDictLearnerV2(object):
             max_iter=10000)
 
         # recording
-        if self.arch_gates is None:
-            self.arch_gates = gates
+        if self.arch_code is None:
+            self.arch_code = code
         else:
-            self.arch_gates = np.vstack([self.arch_gates, gates])
+            self.arch_code = np.vstack([self.arch_code, code])
 
-        if self.scale_code:
-            scaled_gates = self._scale_coeffs(gates)
-            assert np.max(scaled_gates) == 1.0
+        if self._scale_code:
+            scaled_code = self._scale_coeffs(code)
+            assert np.max(scaled_code) == 1.0
         else:
-            scaled_gates = gates
+            scaled_code = code
         
         if self._verbose:
-            recon = np.dot(gates, self.D)
-            print('Gates Learning Stage')
-            print(f'Rate of deactivate: {np.mean(gates <= 0):.4f}')
-            print(f'Rate of activate: {np.mean(scaled_gates > 0):.4f}')
+            recon = np.dot(code, self.D)
+            print('Spare Coding of Task Embedding')
+            print(f'Rate of deactivate: {1-np.mean(np.heaviside(scaled_code, 0)):.4f}')
+            print(f'Rate of activate: {np.mean(np.heaviside(scaled_code, 0)):.4f}')
             print(f'Recontruction loss: {np.mean((sample - recon)**2):.4e}')
             print('----------------------------------')
 
-        return scaled_gates
+        return scaled_code
 
     def update_dict(self, codes: np.ndarray, sample: np.ndarray):
         self.N += 1
-        if self.scale_code:
+        if self._scale_code:
             codes = self._rescale_coeffs(codes)
         # recording
         if self.C is None:
@@ -321,22 +323,23 @@ class OnlineDictLearnerV2(object):
         assert self.C.shape[0] == self.N
             
         # Update the auxiliary variables
-        batch_size = 1
-        if self.N < batch_size - 1:
-            theta = float((self.N + 1) * batch_size)
-        else:
-            theta = float(batch_size**2 + self.N + 1 - batch_size)
-        beta = (theta + 1 - batch_size) / (theta + 1)
+        # Improvement for mini-batch version
+        # batch_size = 1
+        # if self.N < batch_size - 1:
+        #     theta = float((self.N + 1) * batch_size)
+        # else:
+        #     theta = float(batch_size**2 + self.N + 1 - batch_size)
+        # beta = (theta + 1 - batch_size) / (theta + 1)
 
-        self.A *= beta
+        # self.A *= beta
         self.A += np.dot(codes.T, codes)
-        self.B *= beta
+        # self.B *= beta
         self.B += np.dot(sample.T, codes)
 
         # pre-verbose
         if self._verbose:
             recons = np.dot(self.C, self.D)
-            print('Dicts Learning Stage')
+            print('Dictionary Learning')
             print(f'Pre-MSE loss: {np.mean((self.archives - recons)**2):.4e}')
 
         old_D = deepcopy(self.D)
@@ -351,7 +354,7 @@ class OnlineDictLearnerV2(object):
             self.c,
             verbose=self._verbose,
             random_state=self.rng,
-            positive=self._positive_code,
+            positive=self._positive_code
         )
         
         self.change_of_dict.append(np.linalg.norm(old_D - self.D)**2/self.D.size)
@@ -385,19 +388,14 @@ class OnlineDictLearnerV2(object):
 
     def _compute_corr_matrix(self, pre=True):
         if pre:
-            corr_matrix = cosine_similarity(self.arch_gates)
+            corr_matrix = cosine_similarity(self.arch_code)
         else:
             corr_matrix = cosine_similarity(self.C)
-
-        # compute correlation matrix
-        # dist_matrix -= np.min(dist_matrix)
-        # dist_matrix /= np.max(dist_matrix)
-        # corr_matrix = 1 - dist_matrix
 
         return corr_matrix
 
     def _compute_overlapping(self):
-        binary_masks = np.heaviside(self.arch_gates, 0)
+        binary_masks = np.heaviside(self.arch_code, 0)
         overlap_mat = np.empty((binary_masks.shape[0], binary_masks.shape[0]))
 
         for i in range(binary_masks.shape[0]):
@@ -447,28 +445,28 @@ if __name__ == "__main__":
         'Pick and place a puck onto a shelf.',
         'Push and close a window.',
         'Unplug a peg sideways.',
-        # 'Hammer a screw on the wall.',
-        # 'Bypass a wall and push a puck to a goal.',
-        # 'Rotate the faucet clockwise.',
-        # 'Pull a puck to a goal.',
-        # 'Grasp a stick and pull a box with the stick.',
-        # 'Press a handle down sideways.',
-        # 'Push the puck to a goal.',
-        # 'Pick and place a puck onto a shelf.',
-        # 'Push and close a window.',
-        # 'Unplug a peg sideways.'
+        'Hammer a screw on the wall.',
+        'Bypass a wall and push a puck to a goal.',
+        'Rotate the faucet clockwise.',
+        'Pull a puck to a goal.',
+        'Grasp a stick and pull a box with the stick.',
+        'Press a handle down sideways.',
+        'Push the puck to a goal.',
+        'Pick and place a puck onto a shelf.',
+        'Push and close a window.',
+        'Unplug a peg sideways.'
     ]
     task_idx = [
         'task 1', 'task 2', 'task 3', 'task 4', 'task 5',
         'task 6', 'task 7', 'task 8', 'task 9', 'task 10',
-        # 'task 11', 'task 12', 'task 13', 'task 14', 'task 15',
-        # 'task 15', 'task 17', 'task 18', 'task 19', 'task 20'
+        'task 11', 'task 12', 'task 13', 'task 14', 'task 15',
+        'task 15', 'task 17', 'task 18', 'task 19', 'task 20'
     ]
 
     dict_learner = OnlineDictLearnerV2(
         n_features=384,
         n_components=1024,
-        seed=2,
+        seed=0,
         c=1.0,
         alpha=1e-3,
         method='lasso_lars',
@@ -481,42 +479,42 @@ if __name__ == "__main__":
         print(idx+1, hint_task)
         task_embedding = model.encode(hint_task)
 
-        # compute gates for current task
-        gates = dict_learner.get_gates(task_embedding[np.newaxis, :])
+        # compute code for current task
+        code = dict_learner.get_alpha(task_embedding[np.newaxis, :])
 
         # mimic RL finetuning
-        gates += np.random.normal(size=gates.shape) * 0.1
-        # gates = np.clip(gates, 0, 1.0)
+        code += np.random.normal(size=code.shape) * 1.0
+        code = np.clip(code, -1.0, 1.0)
 
         # online update dictionary via CD
-        dict_learner.update_dict(gates, task_embedding[np.newaxis, :])
+        dict_learner.update_dict(code, task_embedding[np.newaxis, :])
 
     # print(dict_learner._compute_corr_matrix(pre=True))
     # print(dict_learner.change_of_dict)
     # mimic testing stage
-    # res_gates = []
+    # res_code = []
     # for idx, hint_task in enumerate(hints):
     #     print(idx+1, hint_task)
     #     task_embedding = model.encode(hint_task)
-    #     # compute gates for current task
-    #     gates = dict_learner.get_gates(task_embedding[np.newaxis, :])
-    #     res_gates.append(gates)
-    # testing_gates = np.vstack(res_gates)
+    #     # compute code for current task
+    #     code = dict_learner.get_code(task_embedding[np.newaxis, :])
+    #     res_code.append(code)
+    # testing_code = np.vstack(res_code)
 
     # plot similarity / correlation
-    # mat = dict_learner._compute_sim_matrix(testing_gates)
+    # mat = dict_learner._compute_sim_matrix(testing_code)
     # mat = dict_learner._compute_corr_matrix()
-    mat = dict_learner._compute_overlapping()
+    # mat = dict_learner._compute_overlapping()
 
-    fig, ax = plt.subplots(figsize=(7, 5)) # 10: (7, 5), 20: (12, 8)
+    # fig, ax = plt.subplots(figsize=(7, 5)) # 10: (7, 5), 20: (12, 8)
 
-    # YlGn, 
-    im, cbar = heatmap(mat, task_idx, task_idx, ax=ax,
-                    cmap="Blues", font_label={'size': 12},
-                    x_label='', y_label='', 
-                    cbarlabel="Prompt Similarity")
-    texts = annotate_heatmap(im, valfmt="{x:.1f}")
+    # # YlGn, 
+    # im, cbar = heatmap(mat, task_idx, task_idx, ax=ax,
+    #                 cmap="Blues", font_label={'size': 12},
+    #                 x_label='', y_label='', 
+    #                 cbarlabel="Prompt Similarity")
+    # texts = annotate_heatmap(im, valfmt="{x:.1f}")
 
-    # fig.tight_layout()
-    plt.savefig('overlap_cw10.svg', bbox_inches='tight', pad_inches=0.02)
+    # # fig.tight_layout()
+    # plt.savefig('overlap_cw10.svg', bbox_inches='tight', pad_inches=0.02)
     
