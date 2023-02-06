aab98ee39cffcb3bf75c25810b06169ed46665f2
OverEuro <yangyijun94@163.com>
2023-02-06 17:56:45 +1100

    ✨ feat(hyperparameter): new param space

--- configs/sac_cotasp.py
+++ configs/sac_cotasp.py
@@ -1,3 +1,4 @@
+import yaml
 import ml_collections
 
 
@@ -21,7 +22,7 @@ def get_config():
 
     config.actor_configs = ml_collections.ConfigDict()
     config.actor_configs.hidden_dims = (1024, 1024, 1024)   
-    config.actor_configs.name_activation = 'celu'          
+    config.actor_configs.name_activation = 'gelu'          
     config.actor_configs.use_rms_norm = False                     
     config.actor_configs.use_layer_norm = False                   
     config.actor_configs.final_fc_init_scale = 1e-3               
@@ -30,16 +31,27 @@ def get_config():
 
     config.critic_configs = ml_collections.ConfigDict()
     config.critic_configs.hidden_dims = (256, 256, 256)      
-    config.critic_configs.name_activation = 'celu'       
+    config.critic_configs.name_activation = 'gelu'       
     config.critic_configs.use_layer_norm = False                  
 
     config.tau = 0.005
     config.init_temperature = 1.0                                 
-    config.target_entropy = -4.0                                  # by default
+    config.target_entropy = -2.0                                  # by default
 
     return config
 
 
 if __name__ == "__main__":
-    kwargs = dict(get_config())
-    print(**kwargs)
+    
+    # kwargs = dict(get_config())
+    # print(kwargs)
+    yaml_dict = get_config().to_dict()
+    with open('sac_cotasp.yaml', 'w') as file:
+        yaml.dump(yaml_dict, file)
+
+    # with open('sac_cotasp.yaml', 'r') as file:
+    #     yaml_dict = yaml.unsafe_load(file)
+
+    # yaml_dict = yaml.unsafe_load(get_config().to_yaml())
+    # config = ml_collections.ConfigDict(yaml_dict)
+    # print(dict(yaml_dict))
